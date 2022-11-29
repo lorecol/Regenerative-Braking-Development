4c8acd499fc19b824e3893e72a224c88823da90e
Paolo Furia <81318686+paolofuria99@users.noreply.github.com>
2022-11-29 17:52:00 +0100

    Create Read_TRACK_DATA.m

---
+++ MATLAB/Read_TRACK_DATA.m
@@ -0,0 +1,35 @@
+%% Clear all
+clc; clear all; close all;
+
+%% Import data from text file
+% Script for importing data from the following HV_VOLTAGE file:
+[file,path] = uigetfile('*.csv');
+if isequal(file,0)
+   disp('User selected Cancel');
+else
+   disp(['User selected ', fullfile(path,file)]);
+end
+
+%% Set up the Import Options and import the data
+% Automatic detect import options
+opts = detectImportOptions(fullfile(path,file));
+opts.PreserveVariableNames=true;
+
+% Import the data
+HVVOLTAGE = readtable(fullfile(path,file), opts);
+
+% Clear temporary variables
+clear opts
+
+%% Plot data
+% Automatic create a figure with a series of subplot. Assuming that the first column is the time
+figure; clf; hold on
+sgtitle(regexprep(file, '_',' '));
+for i=1:(numel(HVVOLTAGE.Properties.VariableNames)-1)
+    subplot(2,2,i)
+    plot(HVVOLTAGE.(HVVOLTAGE.Properties.VariableNames{1}),  HVVOLTAGE.(HVVOLTAGE.Properties.VariableNames{i+1}) )
+    xlabel('Time')
+    ylabel(regexprep(HVVOLTAGE.Properties.VariableNames{i+1}, '_', ' '))  % Convert underscore '_' to
+end
+hold off
+
