df53b02c068e9e6516fb38aebc66fb2ad422678c
lorecol <lorenzo.colturaato@gmail.com>
2023-09-28 10:27:55 +0200

    updated file for parameter identification

--- MATLAB/Parameter_Identification/battery_cell_characterization.m
+++ MATLAB/Parameter_Identification/battery_cell_characterization.m
@@ -26,7 +26,7 @@ addpath(genpath(pwd))
 %% Loading data from HPPC test
 
 % Let the user choose a .mat file where HPPC data are stored
-[file, path] = uigetfile('HPPC_Test/output/*.mat');
+[file, path] = uigetfile('../HPPC_Test/output/*.mat');
 % Check file selection
 if isequal(file, 0)
    disp('No file has been selected! Please select a file.');
@@ -34,24 +34,13 @@ else
    disp(['Selected file: ', fullfile(path, file)]);
 end
 
-%%
-
 % Load the data
 load(file);
 % Extract the data
 data = HPPCMeas;
 
-clear HPPCMeas;
-
-% Se verranno raccolti nuovi dati eseguendo mainHPPC dentro la
-% cartella MATLAB/HPPC_Test non sarà più necessario tagliare il vettore
-% dello SOC, dal momento che verrà estrapolato già con la giusta
-% dimensione
-data.SOC     = data.SOC(data.SOC ~= 0);
-% data.SOC     = data.SOC';
-data.Current = data.Current';
-data.Voltage = data.Voltage';
-data.Time    = data.Time';
+% Clear some data
+clear HPPCMeas path file;
 
 %% Extracting data from dataset
 % Here we extract data from our dataset: in particular, we're interested in
@@ -61,45 +50,45 @@ data.Time    = data.Time';
 
 current = data.Current;         % Current 
 voltage = data.Voltage;         % Voltage 
-time    = data.Time;            % Time 
-SOC     = data.SOC;             % SOC
+time = data.Time;               % Time 
 
-clear data;
+% SOC
+SOC = data.SoC;                 
+SOC = flip(SOC);
+SOC = SOC';
 
-%% Plotting parameters
+%% Plot the collected data
 
+% Plot the current
 figure
-subplot(2,1,1)
+subplot(2, 1, 1)
 plot(time/3600, current)
 xlabel('Time (h)')
 ylabel('Current (A)')
 
-subplot(2,1,2)
+% Plot the voltage
+subplot(2, 1, 2)
 plot(time/3600, voltage)
 xlabel('Time (h)')
 ylabel('Voltage (V)')
 
 %% Cell characterization 
 
-cellCapacity   = 3;                 % Cell capacity [Ah]             
+% Data
+parallel = 4;                       % Battery configuration
+cellCapacity = 4 * parallel;        % Cell capacity [Ah]             
 cellInitialSOC = SOC(1)/100;        % Initial SOC
 
 % Cell properties array
 cell_prop = [cellCapacity; cellInitialSOC];    
 
-% With these values it works, but we don't know why 
-% maxDischargeCurr  = 2;  % Maximum discharging current, [A]
-% maxChargeCurr     = 2.2;  % Maximum charging current, [A]
-% constCurrSweepSOC = 1;  % Current sweep, [A]
-% toleranceVal      = 0.1;   % Tolerance current value, [A]
+maxDischargeCurr = 2;               % [A] Maximum discharge current 
+maxChargeCurr = 2;                  % [A] Maximum charge current
+constCurrSweepSOC = 1;              % [A] Current sweep
 
-maxDischargeCurr  = 2;      % [A] Maximum discharge current 
-maxChargeCurr     = 2;      % [A] Maximum charge current
-constCurrSweepSOC = 1;      % [A] Current sweep
-
-toleranceValChg      = 0.1;     % [A] Current tolerance for charge impulse
-toleranceValDischg   = 0.5;     % [A] Current tolerance for discharge impulse
-toleranceValSOC      = 0.1;     % [A] Current tolerance for SoC sweep
+toleranceValChg = 0.1;              % [A] Current tolerance for charge impulse
+toleranceValDischg = 0.5;           % [A] Current tolerance for discharge impulse
+toleranceValSOC = 0.1;              % [A] Current tolerance for SoC sweep
 
 hppc_protocol = [maxDischargeCurr   ;...
                  maxChargeCurr      ;...
@@ -108,8 +97,8 @@ hppc_protocol = [maxDischargeCurr   ;...
                  toleranceValDischg ;...
                  toleranceValSOC];
 
-numRCpairs      = 1;              % Number of RC pairs in the model
-initialGuess_RC = [1 100];        % Initial values of RC pairs for optimization purposes
+numRCpairs = 1;                     % Number of RC pairs in the model
+initialGuess_RC = [1 100];          % Initial values of RC pairs for optimization purposes
 
 %% Computation
 
