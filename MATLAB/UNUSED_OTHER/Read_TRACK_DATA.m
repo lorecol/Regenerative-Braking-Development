b3624ac63118980a20b0d2fc095f7bb76daa0b84
lorecol <lorenzo.colturaato@gmail.com>
2023-10-02 16:08:47 +0200

    updated code

--- MATLAB/UNUSED_OTHER/Read_TRACK_DATA.m
+++ MATLAB/UNUSED_OTHER/Read_TRACK_DATA.m
@@ -12,26 +12,31 @@ clear all;
 close all;
 clc; 
 
+% Add telemetry data to Matlab path
+addpath(genpath("endurances"));
+
 %% Import data from .csv file
 
 % Current and voltage data can be found in parsed/primary folder
 
 % Choose the file to import data from
-[file,path] = uigetfile('endurances/*.csv');
-if isequal(file,0)
+[file,path] = uigetfile('endurances/end_009/parsed/primary/*.csv');
+
+% Check if the file was succesfully loaded
+if isequal(file, 0)
    disp('User selected Cancel');
 else
-   disp(['Selected file: ', fullfile(path,file)]);
+   disp(['Selected file: ', fullfile(path, file)]);
 end
 
 %% Set up the Import Options and import the data
 
 % Automatic detect import options
-opts = detectImportOptions(fullfile(path,file));
-opts.PreserveVariableNames=true;
+opts = detectImportOptions(fullfile(path, file));
+opts.PreserveVariableNames = true;
 
 % Import the data
-data = readtable(fullfile(path,file), opts);
+data = readtable(fullfile(path, file), opts);
 
 % Clear temporary variables
 clear opts
@@ -60,7 +65,7 @@ clear opts
 % Create a figure with a series of subplots of data stored in the selected 
 % .csv file
 figure; clf; hold on;
-sgtitle(regexprep(file, '_',' '));
+sgtitle(regexprep(file, '_', ' '));
 
 for i=1:(numel(data.Properties.VariableNames) - 1)
 
